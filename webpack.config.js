ff7376f20b4ebdd940d5b6e42bbcb77ced53fe5a
DANIEL JACKSON <dajack05@gmail.com>
2018-09-20 12:09:54 -0400

    update requirements to fit the tutorial 2.0

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,29 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 15000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(mp3|ogg|wav)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+
+                ]
+            }
+        ]
     }
 };
